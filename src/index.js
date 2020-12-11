7b05183b7e248ad087bec734c04d80e76b1f79e3
Alma-byte <legnaneleh@gmail.com>
2020-12-11 12:23:03 -0600

    segundo commit

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import cipher from './cipher.js';
 console.log(cipher);
 
 alert("Todos tus mensajes serán cifrados")
+
+function showavatar(index){
+  localStorage.setItem("avatares", index);
+  window.location.href= "avatar" + index + ".html";
+}
