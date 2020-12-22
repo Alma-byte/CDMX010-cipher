6e841cf5032e64da7a730d557a966b1fc40c3a83
Alma-byte <legnaneleh@gmail.com>
2020-12-22 12:35:59 -0600

    22-12

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,35 @@
+const cipher = {
 
-encode (offset, string){
-  let output= [];
-  for (let i = 0; i < string.length, i++){
-     let ascii= string.charCodeAt(i);
-     let formula= (((ascii-65)+offset%26)+65);
-     output [i]= String.fromCharCode(formula)
+  encode: function (offset, string) {
+    let output= [];
+    
+    for (let i = 0; i < string.length; i++) {
+      let ascii= string.charCodeAt(i);
+      let formula= (((ascii-65)+offset%26)+65);
+      output [i]= String.fromCharCode(formula);
+    }
+
+    return output;
+  },
+
+
+  decode: function (offset, string) {
+    let output= [];
+    
+    for (let i = 0; i < string.length; i++) {
+      let ascii= string.charCodeAt(i);
+      let stringResult= (((ascii-65)-offset%26)+65);
+      output [i]= String.fromCharCode(stringResult);
+    }
+
+    return output;
   }
-  return output;
-}
+   
+};
+
+
+
+export default cipher;
+
+
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+console.log(cipher.encode( 10 ,"adios"))
+console.log(cipher.decode( 10 ,"knsy}"))
 
 //Funcion para mostrar segunda ventana
   function mostrarventana() {
-    document.getElementById("ventana").style.display = "block";
+  document.getElementById("ventana").style.display = "block";
   }
 
   //Constantes
-  const encriptar = document.getElementbyId('botoncodificar')
-  const desencriptar = document.getElementbyId('botondescodificar')
-  const desplazamiento = document.getElementbyId('desplazamiento')
-  const textoEntrada =  document.getElementbyId('mensaje')
-  const textosalida = document.getElementbyId('mensaje2')
+  const encriptar = document.getElementById('botoncodificar')
+  const desencriptar = document.getElementById('botondescodificar')
+  const desplazamiento = document.getElementById('desplazamiento')
+  const textoEntrada =  document.getElementById('mensaje')
+  const textosalida = document.getElementById('mensaje2')
+
+
 
 //Funcion para que aparezca el texto codificado
-  encriptar.addEventListener("click",function(){
-    const textoEntrada= (document.getElementById("mensaje").value.)toUpperCase();
+  encriptar.addEventListener("click", function() {
+    const textoEntrada= document.getElementById("mensaje").value.toUpperCase();
     desplazamiento = Number.parseInt(document.getElementById("desplazamiento").value);
     document.getElementById("mensaje2").value = cifrar(texto,desplazamiento);
   }. true);
@@ -24,7 +27,7 @@ console.log(cipher);
 
   //Funcion para que aparezca el texto decodificado
   encriptar.addEventListener("click",function(){
-    const textoSalida= (document.getElementById("mensaje2").value.)toUpperCase();
+    const textoSalida= document.getElementById("mensaje2").value.toUpperCase();
     desplazamiento = Number.parseInt(document.getElementById("desplazamiento").value);
     document.getElementById("mensaje").value = descifrar(texto,desplazamiento);
   }. true);
